599cb20a599acc874e8630cdf01a0a1d556bf0b2
Siva <56733505+BOT-CODER@users.noreply.github.com>
2020-05-02 15:01:32 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,9 @@
 clear
 echo -e "                \e[31;1mInstalling SniperMan\e[0m"
 echo -e "                \e[34;1mPlease Wait upto 5min\e[0m"
-apt-get -y update > /dev/null 2>&1
-apt-get -y install git > /dev/null 2>&1
+apt-get -y install python > /dev/null 2>&1
 apt-get -y install python3 > /dev/null 2>&1
-python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip > /dev/null 2>&1
 pip install tqdm > /dev/null 2>&1
 pip install pyfiglet > /dev/null 2>&1
 clear
