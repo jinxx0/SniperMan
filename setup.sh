cf8b03315b60a67a909cc65702d49d913c8028c2
Siva <56733505+BOT-CODER@users.noreply.github.com>
2020-05-18 05:03:36 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ apt-get -y install python-pip > /dev/null 2>&1
 apt-get -y install python3-pip > /dev/null 2>&1
 python3 -m pip install --upgrade pip > /dev/null 2>&1
 pip install tqdm > /dev/null 2>&1
-pip install pyfiglet > /dev/null 2>&1
+pip install pyfiglet==0.7 > /dev/null 2>&1
 clear
 echo -e "\e[1;92m[*]SniperMan is installed To run SniperMan type '\e[93mpython3 SniperMan.py'\e[0m "
